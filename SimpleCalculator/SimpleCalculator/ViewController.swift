43c9a77fa0cdf0099d3f999b9dc4c115dce76efc
yusuf <ysfozturk3@gmail.com>
2021-10-13 23:28:16 +0200

    secondlesson-2

--- SimpleCalculator/SimpleCalculator/ViewController.swift
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func addiation(_ sender: Any) {
+        if let firsNum = Int(firstNumber.text!) {
+            if let secondNum = Int(secondNumber.text!) {
+                let res = firsNum + secondNum
+                result.text = String(res)
+            }
+        }
+        
+    
+    }
+    @IBAction func subtraction(_ sender: Any) {
+        
+        if let firsNum = Int(firstNumber.text!) {
+            if let secondNum = Int(secondNumber.text!) {
+                let res = firsNum - secondNum
+                result.text = String(res)
+            }
+        }
+    }
+    
+    @IBAction func Division(_ sender: Any) {
+        
+        if let firsNum = Int(firstNumber.text!) {
+            if let secondNum = Int(secondNumber.text!) {
+                let res = firsNum / secondNum
+                result.text = String(res)
+            }
+        }
+    }
+    
+    
+    @IBAction func multifuction(_ sender: Any) {
+        
+        if let firsNum = Int(firstNumber.text!) {
+            if let secondNum = Int(secondNumber.text!) {
+                let res = firsNum * secondNum
+                result.text = String(res)
+            }
+        }
+    }
 }
 
