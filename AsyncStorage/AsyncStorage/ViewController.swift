f8335df22f537cd29ca236f1a17c58670a89ceaf
yusuf <ysfozturk3@gmail.com>
2021-10-14 23:13:14 +0200

    userDefaults example

---
+++ AsyncStorage/AsyncStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsyncStorage
+//
+//  Created by ysf on 14.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
