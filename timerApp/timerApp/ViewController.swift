6fa4cfb3ebe584f74d24d9dd713c8de5a8c89785
yusuf <ysfozturk3@gmail.com>
2021-10-15 23:50:49 +0200

    timer App main structure

---
+++ timerApp/timerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  timerApp
+//
+//  Created by ysf on 15.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
