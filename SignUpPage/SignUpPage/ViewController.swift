b451078c127e2503b973ada0921102f195a281aa
yusuf <ysfozturk3@gmail.com>
2021-10-15 21:01:23 +0200

    login Page completed

--- SignUpPage/SignUpPage/ViewController.swift
+++ SignUpPage/SignUpPage/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var password2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        if userName.text == "" {
+            alertCreator(title: "Error!", message: "Username not found")
+        } else if Int(password.text!.count) < 6 {
+           alertCreator(title: "Error!", message: "Password cannot be empty or less than 6 character")
+        } else if password.text != password2.text {
+            alertCreator(title: "Error!", message: "Passwords are not match")
+        } else {
+            alertCreator(title: "Success", message: "you can login successfully")
+            
+        }
+        
+    }
+   
+    func alertCreator(title : String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
