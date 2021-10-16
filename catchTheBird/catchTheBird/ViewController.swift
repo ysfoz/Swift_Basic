317e27e5bde1e593ad89c93552ecac50ecc0e1d8
yusuf <ysfozturk3@gmail.com>
2021-10-16 17:05:36 +0200

    started catchTheBird game, created timer

---
+++ catchTheBird/catchTheBird/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catchTheBird
+//
+//  Created by ysf on 16.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
