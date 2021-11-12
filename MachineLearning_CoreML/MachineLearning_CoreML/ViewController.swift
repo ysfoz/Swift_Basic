453303d50ee4e6f2b133dce901bfdffa5921193f
yusuf <ysfozturk3@gmail.com>
2021-11-12 22:07:11 +0100

    .

--- MachineLearning_CoreML/MachineLearning_CoreML/ViewController.swift
+++ MachineLearning_CoreML/MachineLearning_CoreML/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        
     }
 
-
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
