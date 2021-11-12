5c592c5224b242b9d4408f3231b8c01685bd5160
yusuf <ysfozturk3@gmail.com>
2021-11-12 21:50:17 +0100

    created a machine Learning Project

---
+++ MachineLearning_CoreML/MachineLearning_CoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearning_CoreML
+//
+//  Created by ysf on 12.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
